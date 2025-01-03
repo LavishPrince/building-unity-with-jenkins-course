f392a7419ea051bb7db82b09460b9d7ca079214d
Vinay DG <vinay.dg@travancoreanalytics.com>
2025-01-03 11:36:52 +0530

    Add Android Build script

--- jenkins-unity-test/Assets/Editor/Build.cs
+++ jenkins-unity-test/Assets/Editor/Build.cs
@@ -3,18 +3,19 @@ using UnityEditor.Build.Reporting;
 using UnityEngine;
 using System.IO.Compression;
 using System.IO;
+using System;
 
 public class BuildScript
 {
     public static void BuildWindowsCommandLine()
     {
-        // Define the path where the build will be placed.
+        // Define the path where the build will be placed
         string buildPath = "Builds/Windows/MyGame.exe";
 
-        // Create the directory if it doesn't exist.
-        System.IO.Directory.CreateDirectory("Builds/Windows");
+        // Create the directory if it doesn't exist
+        System.IO.Directory.CreateDirectory(Path.GetDirectoryName(buildPath));
 
-        // Get the scenes included in the build settings.
+        // Get the scenes included in the build settings
         string[] scenes = GetScenes();
 
         // Build player options
@@ -40,18 +41,22 @@ public class BuildScript
         {
             Debug.LogError("Build failed!");
         }
+
+        // Zip the build folder
         ZipBuild("Builds/Windows");
     }
 
     private static string[] GetScenes()
     {
-        // Collect all enabled scenes in the build settings.
+        // Collect all enabled scenes in the build settings
         int sceneCount = EditorBuildSettings.scenes.Length;
         string[] scenes = new string[sceneCount];
+
         for (int i = 0; i < sceneCount; i++)
         {
             scenes[i] = EditorBuildSettings.scenes[i].path;
         }
+
         return scenes;
     }
 
@@ -62,7 +67,64 @@ public class BuildScript
         {
             File.Delete(zipPath);
         }
+
+        // Create a zip archive of the build folder
         ZipFile.CreateFromDirectory(buildPath, zipPath);
+    }
+
+    public static void BuildAndroid()
+    {
+        string[] args = Environment.GetCommandLineArgs();
+        string buildType = GetArgument(args, "-buildType");
+
+        string path = buildType switch
+        {
+            "APK" => "Builds/AndroidAPK/TestGame.apk",
+            "AAB" => "Builds/AndroidAAB/TestGame.aab",
+            _ => null
+        };
+
+        if (string.IsNullOrEmpty(path))
+        {
+            Debug.LogError("Invalid or missing build type specified.");
+            return;
+        }
+
+        // Ensure the directory exists
+        System.IO.Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+        // Build player options
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = GetScenes(),
+            locationPathName = path,
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        };
+
+        // Configure Android-specific settings
+        PlayerSettings.Android.minSdkVersion = (AndroidSdkVersions)25;
+        PlayerSettings.Android.targetSdkVersion = (AndroidSdkVersions)34;
+        PlayerSettings.Android.useAPKExpansionFiles = buildType == "AAB";
+        PlayerSettings.Android.useCustomKeystore = false;
+        EditorUserBuildSettings.buildAppBundle = buildType == "AAB";
+
+        // Start the build process
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+    }
+
+    private static string GetArgument(string[] args, string name)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == name && i + 1 < args.Length)
+            {
+                return args[i + 1];
+            }
+        }
+
+        return null;
     }
 }
 
